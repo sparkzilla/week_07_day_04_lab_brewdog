5b203f511bd4635d43b699ee729ef50ad062e82b
Ellie <cassona08@gmail.com>
2019-12-19 11:18:39 +0000

    fetch data from API testedin vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>The Page</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeersList from ('./components/BeersList.vue');
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      beers: []
+    }
+  },
+
+mounted(){
+  fetch('https://api.punkapi.com/v2/beers')
+  .then(res => res.json())
+  .then(beers => this.beers = beers)
+},
+components: {
+  "beers-list": BeersList
+}
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
